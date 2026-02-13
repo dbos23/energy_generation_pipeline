071b1376d53054e90e4227f6cf34e2b87f0de028
Daniel Bostrom <119975555+dbos23@users.noreply.github.com>
2026-02-13 11:10:25 -0500

    Paginated to download all API results

--- extract.py
+++ extract.py
@@ -1,21 +1,45 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
+from datetime import datetime
 
 #load api key
 load_dotenv()
 api_key = os.getenv('api_key')
 
-#define url and request parameters
+#define url for download
 url = 'https://api.eia.gov/v2/electricity/rto/daily-fuel-type-data/data/'
-params = {
-    'api_key': api_key,
-    'frequency': 'daily',
-    'data[0]': 'value',
-    'offset': 0
-}
 
-response = requests.get(url, params=params)
+#create directory for output data and timestamp for output file names
+os.makedirs('data', exist_ok=True)
+current_timestamp = datetime.now()
+current_timestamp_str = current_timestamp.strftime('%Y-%m-%d_%H-%M-%S')
 
-print(response)
-print(response.reason)
+#the results will be paginated. these variables will account for that pagination
+iteration = 0
+offset = 0
+total_results = 100000 #setting this arbitrarily at first to make sure at least one download always runs
+
+while offset < total_results:
+    #define download parameters
+    params = {
+        'api_key': api_key,
+        'frequency': 'daily',
+        'data[0]': 'value',
+        'offset': offset,
+        'length': 5000
+    }
+
+    #download results and write to file
+    response = requests.get(url, params=params)
+    data = response.json()
+    output_file_name = current_timestamp_str + '_' + str(iteration) + '.json'
+
+    with open(f'data/{output_file_name}', 'w') as json_file:
+        json.dump(data, json_file)
+
+    #increment variables and set the total_results variable to its true value. we'll have to paginate the downloads to get them all since one download is limited to 5000
+    iteration += 1
+    offset += 5000
+    total_results = int(data['response']['total'])
