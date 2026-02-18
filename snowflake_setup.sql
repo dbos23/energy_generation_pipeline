ce559eb56fc7d405186f0d7e1ac562b85f23ed18
Daniel Bostrom <119975555+dbos23@users.noreply.github.com>
2026-02-18 15:34:08 -0500

    Added snowflake_setup.sql file

---
+++ snowflake_setup.sql
@@ -0,0 +1,142 @@
+use schema {{ schema }};
+
+--set up stage
+
+
+--connect snowflake with s3
+create storage integration eia_storage_integration
+    type = external_stage
+    storage_provider = s3
+    enabled = true
+    storage_aws_role_arn = '{{ aws_role_arn }}'
+    storage_allowed_locations = ('{{ aws_bucket_url }}')
+;
+
+--storage_aws_iam_user_arn and storage_aws_external_id need to be copied into the trust relationship for the role in AWS
+desc integration eia_storage_integration;
+
+--stage data from s3
+create or replace stage energy_generation
+    url = '{{ aws_bucket_url }}'
+    storage_integration = eia_storage_integration
+    file_format = (
+        type = 'JSON'
+        strip_outer_array = true
+);
+
+--view all files in the s3 bucket to confirm storage integration is working properly
+list @energy_generation;
+
+
+
+
+
+--------------------
+--set up tables and stream
+
+
+--create raw table
+create table eia_raw (
+  json_data variant,
+  file_name varchar
+);
+
+--create a stream to use to process only newly added data
+create stream eia_stream
+on table eia_raw
+;
+
+--create fact table
+create table fct_energy_outputs (
+    period date,
+    fuel_type_code varchar,
+    fuel_type_name varchar,
+    respondent_code varchar,
+    respondent_name varchar,
+    timezone varchar,
+    timezone_description varchar,
+    amount_generated int,
+    units varchar
+);
+
+
+
+
+
+--------------------
+--create stored procedure to refresh all tables
+create or replace procedure refresh_eia()
+returns varchar
+language sql
+as
+$$
+begin
+
+--------------------
+
+--insert new files from stage into raw table
+copy into eia_raw
+from (
+    select
+        $1,
+        metadata$filename
+    from @energy_generation
+)
+file_format = (
+    type = 'JSON',
+    strip_outer_array = true
+);
+
+--------------------
+
+insert into fct_energy_outputs (
+    select
+        value:period::date as period,
+        value:fueltype::varchar as fuel_type_code,
+        value:"type-name"::varchar as fuel_type_name,
+        value:respondent::varchar as respondent_code,
+        value:"respondent-name"::varchar as respondent_name,
+        value:timezone::varchar as timezone,
+        value:"timezone-description"::varchar as timezone_description,
+        value:value::int as amount_generated,
+        value:"value-units"::varchar as units,
+    from eia_stream,
+        lateral flatten(input => json_data:response:data)
+);
+
+--------------------
+
+create or replace table monthly_energy_generation as (
+    select
+        date_trunc('month', period) as month,
+        fuel_type_code,
+        fuel_type_name,
+        respondent_code,
+        respondent_name,
+        units,
+        sum(amount_generated) as amount_generated
+    from fct_energy_outputs
+    group by all
+    order by
+        month asc,
+        amount_generated desc
+);
+
+return 'All tables updated';
+
+end;
+$$;
+
+
+
+--create task to run the stored procedure at 11:25am every day (but only if the stream has data)
+create or replace task run_eia_pipeline
+warehouse = {{ warehouse }}
+schedule = 'using cron 25 16 * * * UTC' --runs at 4:25pm UTC / 11:25am Eastern
+when system$stream_has_data('eia_stream')
+as
+call refresh_eia()
+;
+
+--activate task
+alter task run_eia_pipeline resume;
